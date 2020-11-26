188280ec6dc5e9af69fafd01c31811106c37b8b8
yuwen chang <provence5058@gmail.com>
2020-11-26 12:30:59 +0800

    feat: modify recordSeeder.js

--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -1,10 +1,19 @@
 const mongoose = require('mongoose')
+const recordList = require('../records.json')
 const Record = require('../record')
-mongoose.connect('mongodb://localhost/expense-tracker', { useNewUrlParser: true, useUnifiedTopology: true }) //資料庫伺服器內的專案名稱
+mongoose.connect('mongodb://localhost/expense-list', { useNewUrlParser: true, useUnifiedTopology: true }) //資料庫伺服器內的專案名稱
 const db = mongoose.connection //取得資料庫的連線狀態
 db.on('error', () => { //連線異常
   console.log('mongodb error!')
 })
 db.once('open', () => { // 連線成功
-  console.log('mongodb connected!')
+  recordList.forEach((item) => {
+    Record.create({// 寫入種子資料
+      name: item.name,
+      category: item.category,
+      date: item.date,
+      amount: item.amount
+    })
+  })
+  console.log('done!')
 })
