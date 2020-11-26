d357ac0019a6777b4c67485d3522a9a03de27681
yuwen chang <provence5058@gmail.com>
2020-11-26 21:28:32 +0800

    feat: modify index page

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
-const Expense = require('./models/record')
+const Record = require('./models/record')
+const Category = require('./models/category')
 const port = 3000
 const app = express()
 
 // 連線到資料庫
-mongoose.connect('mongodb://localhost/expense-tracker', { useNewUrlParser: true, useUnifiedTopology: true }) //資料庫伺服器內的專案名稱
+mongoose.connect('mongodb://localhost/expense-list', { useNewUrlParser: true, useUnifiedTopology: true }) //資料庫伺服器內的專案名稱
 const db = mongoose.connection //取得資料庫的連線狀態
 db.on('error', () => { //連線異常
   console.log('mongodb error!')
@@ -23,10 +24,25 @@ app.use(express.static('public'))
 
 //路由設定
 app.get('/', (req, res) => {
-  Expense.find()
+  let categoryList = []
+  Category.find()
     .lean()
-    .then(expense => res.render('index', { expense, css: 'index.css' }))
-    .catch(error => console.log(error))
+    .then((category) => {
+      // categoryList.push(category)
+      // console.log(category)
+      Record.find()
+        .lean()
+        .then((record) => { // 在category中尋找符合此record項目的icon
+          // console.log(record)
+          if (category.find(item => item.categoryName === record.category)) {
+            console.log(item)
+            record.category = category[category.indexOf(record.category)].icon
+            console.log(record.category)
+          }
+          res.render('index', { record, css: 'index.css' })
+        })
+
+    }).catch(error => console.log(error))
 })
 
 // create
