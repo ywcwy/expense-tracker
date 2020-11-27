922db29f53a67aa75fcf25c9dd6312df40fddb48
yuwen chang <provence5058@gmail.com>
2020-11-27 13:19:36 +0800

    feat: delete function

--- app.js
+++ app.js
@@ -89,6 +89,16 @@ app.post('/expense/:id/edit', (req, res) => {
     .catch(error => console.log(error))
 })
 
+// delete
+app.post('/expense/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Record.findById(id)
+    .then((record) => record.remove())
+    .then(res.redirect(`/`))
+    .catch(error => console.log(error))
+})
+
+
 
 app.listen(port, () => {
   console.log(`now is on localhost:${port}`)
