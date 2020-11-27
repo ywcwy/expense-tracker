31f753b95db32c4181beb2b8e78e022ed2fdd3e2
yuwen chang <provence5058@gmail.com>
2020-11-27 11:22:16 +0800

    feat: modify new page & add create function

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 const Record = require('./models/record')
 const Category = require('./models/category')
 const port = 3000
@@ -20,38 +21,50 @@ db.once('open', () => { // 連線成功
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 // setting static files
-app.use(express.static('public'))
+app.use(express.static('public'), bodyParser.urlencoded({ extended: true }))
 
 //路由設定
 app.get('/', (req, res) => {
-  let categoryList = []
-  Category.find()
+  Record.find()
     .lean()
-    .then((category) => {
-      // categoryList.push(category)
-      // console.log(category)
-      Record.find()
-        .lean()
-        .then((record) => { // 在category中尋找符合此record項目的icon
-          // console.log(record)
-          if (category.find(item => item.categoryName === record.category)) {
-            console.log(item)
-            record.category = category[category.indexOf(record.category)].icon
-            console.log(record.category)
-          }
-          let totalAmount = 0
-          record.forEach(item => {
-            totalAmount += item.amount
-          })
-          res.render('index', { record, totalAmount, css: 'index.css' })
-        })
-
+    .then((record) => {
+      let totalAmount = 0
+      record.forEach(item => {
+        totalAmount += item.amount
+      })
+      res.render('index', { record, totalAmount, css: 'index.css' })
     }).catch(error => console.log(error))
 })
 
+
 // create
-app.get('/expense/new', (req, res) => {
-  res.render('new', { css: 'edit.css' })
+app.get('/expense/new', (req, res) => { // new page
+  return res.render('new', { css: 'edit.css' })
+})
+
+app.post('/expense', (req, res) => { // 將 new page 填完的資料 post
+  const body = req.body
+  const record = new Record({  // 從 Record 產生一個實例
+    name: body.name,
+    category: body.category,
+    icon: body.category,
+    date: body.date,
+    amount: body.amount
+  })
+  Category.find({ categoryName: body.category }, (err, categories) => {
+    // 從 Category 中尋找相對應的 icon 值
+    if (err) {
+      return console.error(err)
+    }
+    categories.forEach(category => {
+      console.log(`${category.categoryName} : ${category.icon}`)
+      record.icon = category.icon  // 修改實例中的 icon 值
+    })
+  }).lean()
+    .then(() => {
+      record.save()  // 將實例存入資料庫
+      res.redirect('/')
+    }).catch(error => console.log(error))
 })
 
 app.listen(port, () => {
