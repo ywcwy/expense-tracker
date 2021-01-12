c763ae38a0131adf8104b8baece19cbdbdafa4c0
yuwen chang <provence5058@gmail.com>
2021-01-12 13:38:23 +0800

    feat: add register page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,16 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/login', (req, res) => {
-  res.render('login')
-})
+router.get('/login', (req, res) => res.render('login'))
 
 router.post('/login', (req, res) => {
 
 })
 
-router.get('/register', (req, res) => {
-  res.render('register')
-})
+router.get('/register', (req, res) => res.render('register'))
 
 module.exports = router
