bbb20cd1529c494ac8e21c8fea688c0049f4aaba
yuwen chang <provence5058@gmail.com>
2021-01-12 17:51:13 +0800

    feat: add logout feature

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,6 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
+const User = require('../models/user')
 
 module.exports = app => {
   // 初始化passport模組
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -2,7 +2,10 @@ const express = require('express')
 const router = express.Router()
 const User = require('../../models/user')
 const passport = require('passport')
+const { rawListeners } = require('../../models/user')
+
 router.get('/login', (req, res) => res.render('login'))
+
 router.post('/login', passport.authenticate('local', {
   failureRedirect: '/users/login',
   successRedirect: '/'
@@ -10,6 +13,7 @@ router.post('/login', passport.authenticate('local', {
 
 
 router.get('/register', (req, res) => res.render('register'))
+
 router.post('/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
   User.findOne({ email })
@@ -28,6 +32,11 @@ router.post('/register', (req, res) => {
         .then(() => res.redirect('/'))
         .catch(err => console.log(err))
     })
+})
+
+router.get('/logout', (req, res) => {
+  req.logout()
+  res.redirect('/users/login')
 })
 
 module.exports = router
