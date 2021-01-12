a93ebc477fd62e5193adefa54fe2ca26106b97bf
yuwen chang <provence5058@gmail.com>
2021-01-12 14:44:06 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const session = require('express-session')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
@@ -16,6 +17,12 @@ app.set('view engine', 'handlebars')
 // setting static files, bodyParse, methodOverride
 app.use(express.static('public'), bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.use(session({
+  secret: 'ThisIsMySecret',  // 用來驗證 session id 的字串
+  resave: false,  //  true 時，會在每一次與使用者互動後，強制把 session 更新到 session store 裡。
+  saveUninitialized: true // 強制將未初始化的 session 存回 session store。未初始化表示這個 session 是新的而且沒有被修改過，例如未登入的使用者的 session。
+
+}))
 
 app.use(routes)
 
